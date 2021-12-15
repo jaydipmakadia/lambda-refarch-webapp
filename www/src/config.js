066940b301a8f7ecf375d39dac224c983c5d23b7
Jaydip Makadia <jaydip@systemintegration.in>
2021-12-15 19:01:48 +0530

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,7 +2,7 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_cognito_region": "us-east-1",
+  "aws_cognito_region": "ap-south-1",
   "aws_user_pools_id": "None",                           // CognitoID
   "aws_user_pools_web_client_id": "None",     // CognitoClientID
   "api_base_url": "None",                                     // TodoFunctionApi
