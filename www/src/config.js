391c55123bf0a683c25ac057be85382303e373e7
Jaydip Makadia <jaydip@systemintegration.in>
2021-12-15 19:11:56 +0530

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -3,11 +3,11 @@
 
 const config = {
   "aws_cognito_region": "ap-south-1",
-  "aws_user_pools_id": "None",                           // CognitoID
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_id": "ap-south-1_nDwp5PGi5",                           // CognitoID
+  "aws_user_pools_web_client_id": "49dm05jhhk8kvqef7ktu47ofc8",     // CognitoClientID
+  "api_base_url": "https://pkvgqx2b90.execute-api.ap-south-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "mytodoappdemo-jaydip-lambda-refarch-webapp.auth.ap-south-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://amplify-console.d1mbe09cqwc6qv.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
